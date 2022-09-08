9a82469c4c506dc538f0a5e2b7e6e7ed3c63fa02
Vikram <vikramd1001@gmail.com>
2022-09-08 17:57:46 +0530

    Added data

---
+++ main.js
@@ -0,0 +1,9 @@
+function randomNumber(num){
+    return Math.floor(Math.random() * num);
+}
+
+const randomVals = {
+    name: ['Zuhura Theodosius', 'Aglaia Manus', 'Emeka Encarnita', 'Dottie Octavia', 'Jagdish Darayavahush', 'Ratimir Gronw', 'Libuše Esmée', 'Octavius Bishop', 'Candelas Jyotsna', 'Inayat Aniela'],
+    age: [20, 18, 15, 27, 30, 40, 45, 22, 60, 35],
+    phone: [2818756411, 920860173, 3862834688, 5743485543, 6418650186, 5417355030, 6082423143, 2057375350, 6627060317, 9064974218]
+}
