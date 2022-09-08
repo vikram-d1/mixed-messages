008ce3cabd01c07c22e5d52c93dcf5e23334cd56
Vikram <vikramd1001@gmail.com>
2022-09-08 18:16:11 +0530

    Added loop

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ const randomVals = {
     age: [20, 18, 15, 27, 30, 40, 45, 22, 60, 35],
     phone: [2818756411, 920860173, 3862834688, 5743485543, 6418650186, 5417355030, 6082423143, 2057375350, 6627060317, 9064974218]
 }
+
+let generated = [];
+
+for(let data in randomVals){
+    let rand = randomNumber(randomVals[data].length);
+
+    switch(data){
+        case 'name':
+            generated.push(randomVals['name'][rand]);
+            break;
+
+        case 'age':
+            generated.push(randomVals['age'][rand]);
+            break;
+
+        case 'phone':
+            generated.push(randomVals['phone'][rand]);
+            break;
+    }
+}
